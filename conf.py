23c8dfe2296d80060ff193bdac4f0ba4df4c48fb
NassiraNguadii <133218646+NassiraNguadii@users.noreply.github.com>
2024-12-03 17:15:44 +0100

    Update conf.py

--- conf.py
+++ conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Textra'
+project = 'Textra-Health'
 copyright = '2024, GIIA'
 author = 'GIIA'
 
